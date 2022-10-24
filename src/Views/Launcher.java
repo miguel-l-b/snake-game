2e219c3a8b3e43ef704ac501a89f34153643d885
to-voando <miguelcoruj@gmail.com>
2022-10-23 21:48:37 -0300

    visual interface

--- src/Views/Launcher.java
+++ src/Views/Launcher.java
@@ -2,6 +2,38 @@ package Views;
 
 import javax.swing.*;
 
-public class Launcher extends JFrame {
-    
+import Console.ConsoleManager;
+
+import java.awt.event.ActionEvent;
+
+import utils.Window;
+
+public class Launcher extends Window {
+    private JLabel lbl_username = new JLabel("Escolha um nome de usuário:");
+    private JTextField txt_username = new JTextField();
+    private JButton btn_connection = new JButton("Conectar");
+
+    public Launcher() {  
+        super("Jogo da Cobra", "main");
+        super.setSize(270, 160);
+        super.centralize();
+        super.start();
+        super.closeProgramOnClose();
+        super.setResizable(false);
+
+        super.setLayout(null);
+        super.add(lbl_username);
+        super.add(txt_username);
+        super.add(btn_connection);
+
+        lbl_username.setBounds(10, 15, 250, 20);
+        txt_username.setBounds(10, 45, 250, 25);
+        btn_connection.setBounds(160, 80, 100, 30);
+
+        btn_connection.addActionListener((e) -> { btnConnectionHandler(e); });
+    }
+
+    public void btnConnectionHandler(ActionEvent e) {
+        ConsoleManager.println(txt_username.getText());
+    }
 }
