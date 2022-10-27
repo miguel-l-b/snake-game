118eb0bcfc29729a55d265b752007f66098f5d5b
to-voando <miguelcoruj@gmail.com>
2022-10-27 10:27:41 -0300

    methods to move the player

--- src/game/Player.java
+++ src/game/Player.java
@@ -18,6 +18,23 @@ public class Player extends Coordinate {
     { this.points = points; }
     protected void addPoint(int points)
     { this.points += points; }
+
+    protected boolean movingHorizontal(int x) {
+        if(x == 0) return false;
+
+        if(x > 0) super.setPosition(super.getX()+1, super.getY());
+        if(x < 0) super.setPosition(super.getX()-1, super.getY());
+
+        return true;
+    }
+    protected boolean movingVertical(int y) {
+        if(y == 0) return false;
+
+        if(y > 0) super.setPosition(super.getX(), super.getY()+1);
+        if(y < 0) super.setPosition(super.getX(), super.getY()-1);
+
+        return true;
+    }
     
     public int getPoints() {return this.points; }
 }
