bc892d43e6ae9cd40cef9c0b7540d18f2bac9534
to-voando <miguelcoruj@gmail.com>
2022-11-12 17:55:02 -0300

    update visual

--- src/Views/GameCanvas.java
+++ src/Views/GameCanvas.java
@@ -10,8 +10,9 @@ import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 
 import game.*;
+import utils.Colors;
 
-public class GameCanvas extends Canvas implements Runnable, KeyListener{
+public class GameCanvas extends Canvas implements Runnable, KeyListener {
     public GameController game;
     private char directions = ' ';
     private boolean running = false;
@@ -19,9 +20,7 @@ public class GameCanvas extends Canvas implements Runnable, KeyListener{
     
     public GameCanvas() throws Exception {
         game = new GameController(20);
-        game.addPlayer(new Player("id","vedras",Color.blue, 0, 0));
-        game.addApple(new Apple("id",5,8,10,-10,500));
-        this.setPreferredSize(new Dimension(600,440));
+        this.setPreferredSize(new Dimension(401,540));
         this.addKeyListener(this);
         handleFrames = new Thread(this);
     }
@@ -52,26 +51,60 @@ public class GameCanvas extends Canvas implements Runnable, KeyListener{
             return;
         }
         Graphics g = bs.getDrawGraphics();
-        g.setColor(Color.black);
-        g.fillRect(0,0,401,440);
-        g.setColor(Color.gray);
-        g.fillRect(401, 0, 200, 440);
+        g.setColor(Colors.BLACK);
+        g.fillRect(0,0,401,402);
+        g.setColor(Colors.WHITE);
+        g.fillRect(0, 402, 401, 138);
         
+
+        var players = game.getPlayers();
         
-        for (Player p : game.getPlayers()) {
+        for (int i = 0; i < players.length; i++) {
+            var p = players[i];
             g.setColor(p.color);
             g.fillRect(p.getX()*20, p.getY()*20, 20, 20);
             g.setColor(Color.white);
             g.drawString(""+p.getPoints(), p.getX()*20+5, p.getY()*20+15);
+
+            g.setColor(p.color);
+            g.drawString(p.username+":  "+p.getPoints(), 10, 420+(20*i));
+
         }
         
         for (Apple a : game.getApples()) {
-            g.setColor(Color.red);
-            g.fillRect(a.getX()*20, a.getY()*20, 20, 20);
-            g.setColor(Color.white);
-            g.drawString(""+a.getValue(), a.getX()*20+5, a.getY()*20+15);
+            int level = a.getLevel();
 
+            g.setColor(Colors.WHITE);
+            g.drawRect(a.getX()*20+8, a.getY()*20+3, 3, 4);
+
+            switch(level) {
+                case 2: g.setColor(Colors.RED); break;
+                case 1: g.setColor(Colors.ORANGE); break;
+                default: g.setColor(Colors.LIGHT_RED); break;
+            }
+            
+            g.fillRect(a.getX()*20+5, a.getY()*20+6, 10, 10);
+            g.setColor(Colors.WHITE);
+            g.drawRect(a.getX()*20+4, a.getY()*20+5, 11, 11);
+
+            switch(level) {
+                case 2: g.setColor(Colors.GREEN); break;
+                case 1: g.setColor(Colors.DARK_GREEN); break;
+                default: g.setColor(Colors.BROW); break;
+            }
+            g.fillRect(a.getX()*20+9, a.getY()*20+4, 2, 6);
         }
+
+        g.setColor(Colors.BLACK);
+
+        g.drawRect(370, 480, 20, 20);
+        g.drawString("↦", 375, 495);
+        g.drawRect(340, 480, 20, 20);
+        g.drawString("↧", 345, 495);
+        g.drawRect(310, 480, 20, 20);
+        g.drawString("↤", 315, 495);
+        g.drawRect(340, 450, 20, 20);
+        g.drawString("↥", 345, 465);
 
         g.dispose();
         bs.show();
@@ -94,7 +127,6 @@ public class GameCanvas extends Canvas implements Runnable, KeyListener{
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
         while(running){
             tick();
             render();
@@ -125,17 +157,8 @@ public class GameCanvas extends Canvas implements Runnable, KeyListener{
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
+    public void keyReleased(KeyEvent e) {}
 
     @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-   
-    
+    public void keyTyped(KeyEvent e) {}
 }
---
+++ src/utils/Colors.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.awt.Color;
+
+public class Colors {
+    public final static Color BROW = new Color(77, 55, 49);
+    public final static Color GREEN = new Color(33, 143, 55);
+    public final static Color DARK_GREEN = new Color(48, 56, 40);
+    public final static Color RED = new Color(201, 12, 12);
+    public final static Color LIGHT_RED = new Color(189, 160, 160);
+    public final static Color ORANGE = new Color(199, 63, 26);
+    public final static Color WHITE = new Color(223, 227, 235);
+    public final static Color BLACK = new Color(41, 45, 54);
+    public final static Color DARK_BLACK = new Color(41, 42, 43);
+
+    public final static Color[] RANDOM = {
+        // BLUE
+        new Color(42, 78, 112),
+        new Color(7, 121, 242),
+
+        // PURPLE
+        new Color(117, 7, 242),
+        // PINK
+        new Color(196, 114, 158),
+
+        // GREEN
+        new Color(81, 150, 12),
+    };
+}
