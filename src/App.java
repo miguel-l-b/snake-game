462a105f2ad0a2f2e5bcfbaba254cc8b5b851bae
to-voando <miguelcoruj@gmail.com>
2022-11-14 11:21:57 -0300

    Co-authored-by: JOAO VICTOR VEDRONI PEREIRA DA SILVA <cc22313@m.unicamp.br>

--- src/App.java
+++ src/App.java
@@ -6,6 +6,6 @@ import Views.Launcher;
 public class App {
     public static void main(String[] args) throws Exception {
         ConsoleManager.println(Colors.GREEN, "started");
-        new Game();
+        new Launcher();
     }
 }
