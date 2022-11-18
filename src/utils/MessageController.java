8c88a3d4d0bee6195828292f79bb5d34cf93ed13
to-voando <miguelcoruj@gmail.com>
2022-11-18 11:02:24 -0300

    reformulate

--- src/utils/MessageController.java
+++ src/utils/MessageController.java
@@ -22,20 +22,17 @@ public class MessageController {
 
     public String getID() { return socket.getRemoteSocketAddress().toString(); }
 
-    public Object getObject() {
-        try {
-            return in.readObject();
-        } catch(Exception err) { err.printStackTrace(); return null; }
+    public Communicate getObject() {
+        try { return (Communicate)in.readObject(); } 
+        catch(Exception err) { return null; }
     }
 
-    public boolean sendObject(Communicate message) {
+    public boolean sendObject(Communicate object) {
         try {
-            out.writeObject(message);
+            out.writeObject(object);
             out.flush();
             return true;
-        } catch (IOException e) {
-            return false;
-        }
+        } catch (Exception e) { return false; }
     }
 
     public void close() throws Exception {
